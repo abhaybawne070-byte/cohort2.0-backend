0781474cde3f0ada4746bef26da7eb76effefcbf
abhay bawne <abhaybawne070@gmail.com>
2026-02-14 00:13:29 +0530

    Added Day08

--- Day08/backend/src/app.js
+++ Day08/backend/src/app.js
@@ -44,9 +44,9 @@ app.delete("/api/notes/:id",async(req,res)=>{
 
 app.patch("/api/notes/:id",async(req,res)=>{
     const id=req.params.id
-    const {description}=req.body
-
-    await noteModel.findByIdAndUpdate(id,{description})
+    const {title ,description}=req.body
+    
+    await noteModel.findByIdAndUpdate(id,{title,description})
 
     res.status(200).json({
         message:"note update successfully"
