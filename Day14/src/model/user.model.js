3dd1e543aebfeb2295a52c2671ae34e55bc30958
abhay bawne <abhaybawne070@gmail.com>
2026-02-25 23:52:57 +0530

    change some code

--- Day14/src/model/user.model.js
+++ Day14/src/model/user.model.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     username:{
         type: String,
-        unique:[ture ,"user name already exist"],
-        required:[ture,"user name already required"]
+        unique:[true ,"user name already exist"],
+        required:[true,"user name already required"]
     },
     email:{
         type: String,
-        unique:[ture,'user email already exist'],
-        required:[ture,"user email already required"]
+        unique:[true,'user email already exist'],
+        required:[true,"user email already required"]
     },
     passward:{
         type:String,
-        require:[ture,'user passward already required']
+        require:[true,'user passward already required']
     },
     bio: String ,
     profileImage:{
@@ -21,3 +21,7 @@ const userSchema = new mongoose.Schema({
         default:'https://ik.imagekit.io/uxfqiaylgl/360_F_586915596_gPqgxPdgdJ4OXjv6GCcDWNxTjKDWZ3JD.jpg'
     }
 })
+
+const userModel = mongoose.model("users",userSchema)
+
+module.exports = userModel
---
+++ Day14/src/routes/auth.routes.js
@@ -0,0 +1,117 @@
+const express = require("express")
+const userModel = require("../model/user.model")
+const crypto = require("crypto")
+const jwt = require("jsonwebtoken")
+
+const authRouter = express.Router()
+
+authRouter.post("/register",async(req,res)=>{
+    const {email,username,password,bio,profileImage} = req.body
+
+    // const isUserExistsByEmail = await userModel.findOne({email})
+
+    // if(isUserExistsByEmail){
+    //     return res.status(409).json({
+    //         messsage:"user already exist with same email"
+    //     })
+    // }
+
+    // const isUserExistsByUsername = await userModel.findOne({username})
+
+    // if (isUserExistsByUsername){
+    //     return res.status(409).json({
+    //         message:"user already exist with same name"
+    //     })
+    // }
+
+    const isUserAlreadyExists = await userModel.findOne({
+    $or:[ 
+            {username},
+            {email}
+        ]
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+             messsage:"User already exist" + (isUserAlreadyExists.email ==
+             email ? "Email already exists" : "Username already exists")
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex")
+
+    const user = await userModel.create({
+        username,
+        email,
+        bio,
+        profileImage,
+        password:hash
+    }) 
+    // user ka data ho
+    // dat unique ho
+    const token = jwt.sign({
+        id: user._id
+    },process.env.JWT_SECRET,
+        {expiresIn:"1d"})
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"user registered successfully",
+        user:{
+            email:user.email,
+            username:user.username,
+            bio:user.bio,
+            profileImage:user.profileImage
+        }
+    })
+})
+
+authRouter.post("/login", async(req,res) =>{
+    const {username,email,password} = req.body
+
+    const user = await userModel.findOne({
+        $or:[
+            {
+                username: username
+            },
+            {
+                email:email
+            }
+        ]
+    })
+    if(!user){
+        return res.status(404).json({
+            message:"user not found"
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex")
+    
+    const isPasswordValid = hash === user.password
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message:"password invalid"
+        })
+    }
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn:"2d"}
+    )
+    res.cookie("token",token)
+
+    res.status(200)
+    .json ({
+        message:"user logged in successfully",
+        user:{
+            username:user.username,
+            email:user.email,
+            bio:user.bio,
+            profileImage:user.profileImage
+        }
+    })
+})
+module.exports = authRouter
