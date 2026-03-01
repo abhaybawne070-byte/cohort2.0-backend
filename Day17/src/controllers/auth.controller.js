211739bcf8eb10f15910f36425e11894e8fe12ee
abhay bawne <abhaybawne070@gmail.com>
2026-03-01 17:20:09 +0530

    feat: initialize Day17 project with Express, MongoDB, and user authentication
    - Added package.json for project dependencies and scripts
    - Created server.js to set up Express server and connect to MongoDB
    - Implemented app.js to configure middleware and routes
    - Established database connection in database.js
    - Developed user authentication controllers in auth.controller.js
    - Created post management controllers in post.controller.js
    - Added user and post models for MongoDB schema
    - Implemented authentication middleware for protected routes
    - Set up routes for user authentication and post management

---
+++ Day17/src/controllers/auth.controller.js
@@ -0,0 +1,117 @@
+const userModel = require("../model/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+async function registerController (req,res){
+    const {email,username,password,bio,profileImage} = req.body
+
+    // const isUserExistsByEmail = await userModel.findOne({email})
+
+    // if(isUserExistsByEmail){
+    //     return res.status(409).json({
+    //         messsage:"user already exist with same email"
+    //     })
+    // }
+
+    // const isUserExistsByUsername = await userModel.findOne({username})
+
+    // if (isUserExistsByUsername){
+    //     return res.status(409).json({
+    //         message:"user already exist with same name"
+    //     })
+    // }
+
+    const isUserAlreadyExists = await userModel.findOne({
+    $or:[ 
+            {username},
+            {email}
+        ]
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+             message:"User already exist" + (isUserAlreadyExists.email ==
+             email ? "Email already exists" : "Username already exists")
+        })
+    }
+
+     const hash = await bcrypt.hash(password,10 ) // solt = kitni bar hashing karna hai ye batata hai
+
+    const user = await userModel.create({
+        username,
+        email,
+        bio,
+        profileImage,
+        password:hash
+    }) 
+    // user ka data ho
+    // data unique ho
+    const token = jwt.sign({
+        id: user._id
+    },process.env.JWT_SECRET,
+        {expiresIn:"1d"})
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"user registered successfully",
+        user:{
+            email:user.email,
+            username:user.username,
+            bio:user.bio,
+            profileImage:user.profileImage
+        }
+    })
+}
+
+async function loginController(req,res){
+    const {username,email,password} = req.body
+
+    const user = await  userModel.findOne({
+         $or:[
+            {
+                username: username
+            },
+            {
+                email: email
+            }
+         ]
+    })
+
+    if(!user){
+        return res.status(404).json({
+            message:"user not found"
+        })
+    }
+
+     const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message:"password invalid"
+        })
+    }
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn:"1d"}
+    )
+    res.cookie("token",token)
+
+    res.status(200)
+    .json ({
+        message:"user logged in successfully",
+        user:{
+            username:user.username,
+            email:user.email,
+            bio:user.bio,
+            profileImage:user.profileImage
+        }
+    })
+}
+
+module.exports = {
+    registerController,
+    loginController
+}
---
+++ Day17/src/model/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique:[true ,"user name already exist"],
+        required:[true,"user name already required"]
+    },
+    email:{
+        type: String,
+        unique:[true,'user email already exist'],
+        required:[true,"user email already required"]
+    },
+    password:{
+        type:String,
+        required:[true,'user password is required']
+    },
+    // Backward-compat for old documents/typos
+    password:{
+        type:String
+    },
+    bio: String ,
+    profileImage:{
+        type:String,
+        default:'https://ik.imagekit.io/uxfqiaylgl/360_F_586915596_gPqgxPdgdJ4OXjv6GCcDWNxTjKDWZ3JD.jpg'
+    }
+})
+
+const userModel = mongoose.model("users",userSchema)
+
+module.exports = userModel
