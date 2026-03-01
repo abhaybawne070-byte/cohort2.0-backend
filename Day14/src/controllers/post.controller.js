42f31b5a9292a4bbbbad6aeb03d365c03bd181fa
abhay bawne <abhaybawne070@gmail.com>
2026-03-01 14:57:33 +0530

    create api

--- Day14/src/controllers/post.controller.js
+++ Day14/src/controllers/post.controller.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken")
 const imagekit =new ImageKit({
     privateKey:process.env.IMAGEKIT_PRIVATE_KEY
 })
+
 async function createPostController(req,res){
 
     console.log(req.body, req.file)
@@ -46,6 +47,84 @@ async function createPostController(req,res){
     })
 } 
 
+async function  getPostController(req,res){
+
+    const token = req.cookies.token
+
+      if(!token){
+            return res.status(401).json({
+                message:"unauthorized access"
+            })
+        }
+
+    let decoded;
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch{
+        return res.status(401).json({
+            message:"token invalid"
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user:userId
+    })
+
+    res.status(200).json({
+        message:"post fatch successfully",
+        posts
+    })
+}
+
+async function getPostDetailsController(req,res){
+        
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"unauthorized access"
+       })
+    }
+
+    let decoded;
+    try{
+        decoded =jwt.verify(token, process.env.JWT_SECRET)
+    }catch (err){
+        return res.status(401).json({
+            message:"Invalid token"
+        })
+    }
+
+    const userId = decoded.id
+    const postId =req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message:"post not found ."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"forbidden content."
+        })
+    }
+    
+    return res.status(200).json({
+        message:"post fetched successfully",
+        post
+    })
+}
+
  module.exports ={
-     createPostController
+     createPostController,
+     getPostController,
+     getPostDetailsController
+
  }
