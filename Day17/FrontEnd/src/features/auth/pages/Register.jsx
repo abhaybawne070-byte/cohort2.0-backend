3b313705fd6d0c9d139c3df75d766dcef4f1d4c6
abhay bawne <abhaybawne070@gmail.com>
2026-03-12 22:35:26 +0530

    feat: implement registration form with axios for API calls and state management

--- Day17/FrontEnd/src/features/auth/pages/Register.jsx
+++ Day17/FrontEnd/src/features/auth/pages/Register.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router'
+import axios from 'axios'
 
 const register = () => {
+  
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/register",{
+      username,
+      email,
+      password
+    })
+    .then(res => {
+      console.log(res.data)
+    })
+  }
   return (
    <main>
       <div className="form-container">
         <h1>Register</h1>
-        <form >
-          <input type="text" name='username' placeholder='Enter username'/>
-          <input type="email" name='username' placeholder='Enter email' />
-          <input type="password" name='password' placeholder='Enter password' />
+        <form onSubmit={handleSubmit}>
+          <input
+          onInput={(e)=>{setUsername(e.target.value)}}
+          type='text'
+          name='username'
+          placeholder='Enter username'/>
+  
+          <input
+           onInput={(e)=>{setUsername(e.target.value)}}
+           type="email"
+           name='username'
+           placeholder='Enter email' />
+
+          <input 
+           onInput={(e)=>{setUsername(e.target.value)}}
+           type="password"
+           name='password'
+           placeholder='Enter password' />
+
           <button>Register</button>
         </form>
         <p>Already have an account? <Link className='toggleAuthForm' to='/login' >Login</Link></p>
