50af24fb9716de3fbaf665fcc29c2a1d24f7db17
abhay bawne <abhaybawne070@gmail.com>
2026-03-14 00:50:21 +0530

    feat: add getMeController for user profile retrieval and update auth routes

--- Day17/BackEnd/src/routes/auth.routes.js
+++ Day17/BackEnd/src/routes/auth.routes.js
@@ -1,10 +1,23 @@
 const express = require("express")
 const authController = require("../controllers/auth.controller")
+const identifyUser = require("../middlewares/auth.middleware")
 
 const authRouter = express.Router()
 
+/**
+ * post /api/auth/register
+ */
 authRouter.post("/register",authController.registerController)
 
+
+/**
+ * post /api/auth/login
+ */
 authRouter.post("/login",authController.loginController)
+
+/**
+ * @routes Get/api/auth/logout
+ */
+authRouter.get("/get-me",identifyUser, authController.getMeController)
 
 module.exports = authRouter
--- Day17/FrontEnd/src/features/auth/services/auth.api.js
+++ Day17/FrontEnd/src/features/auth/services/auth.api.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
 
+const api = axios.create({
+    baseURL:"http://localhost:3000/api/auth/",
+    withCredentials:true
+}) 
 export async function register(username , email , password){
 
     try{
-        const response = await axios.post("http://localhost:3000/api/auth/register",{
+        const response = await api.post("/register",{
             username,
             email,
             password
-        },{
-            withCredentials:true
         }) 
         return response.data
     }
@@ -21,11 +23,9 @@ export async function register(username , email , password){
 export async function login(email, password){}
 
     try{
-        const response = await axios.post("http://localhost:3000/api/auth/login",{
+        const response = await api.post("/login",{
             username,
             password
-        },{
-            withCredentials:true
         })
         return response.data
     }
