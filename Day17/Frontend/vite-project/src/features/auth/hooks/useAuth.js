8ffe194bd210a6d8c5a33f5354aea40dccce7696
abhay bawne <abhaybawne070@gmail.com>
2026-03-26 23:50:39 +0530

    feat: implement useAuth hook to manage authentication context

--- Day17/Frontend/vite-project/src/features/auth/hooks/useAuth.js
+++ Day17/Frontend/vite-project/src/features/auth/hooks/useAuth.js
@@ -1,3 +1,9 @@
 import { useContext } from "react";
 import { AuthContext } from "../auth.context";
 
+export const useAuth =() =>{
+    
+    const context = useContext(AuthContext)
+
+    const {user, setUser , loading , setLoading} = context
+}
